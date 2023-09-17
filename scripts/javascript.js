e235f83a00981251a515cbfe061293c96a33afc9
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-09-17 13:26:02 +0200

    ulitmo

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,19 +1,6 @@
 
 window.addEventListener('load', async function() {
-console.log("prueba pull");
-    const listaPokemon = document.getElementById('listaPokemon');
-    const tipoPokemon = document.getElementById('tipoPokemon');
-    const botonesHeader = document.querySelectorAll('.button__nav');
-    let botonesFooter = new Array();
 
-    const listaFooter = document.getElementById('footer__ul');
-
-    const cantPorPagina = 20;
-
-    let url = "http://pokeapi.co/api/v2/";
-
-
-    
 
     let usandose = false;
     let cantidadMostrada = 0;
